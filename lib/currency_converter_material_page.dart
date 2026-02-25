ed5e130eb18c42b8456e77a8213f2c3c566045b6
zgirto-hub <zgirto@gmail.com>
2026-02-25 12:54:37 +0300

    25-feb-2026 12:54

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,8 +4,17 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
   const CurrencyConverterMaterialPage({super.key});
 
   @override
-  Widget build(BuildContext context) {    
-    return  const  Scaffold(
+  Widget build(BuildContext context) {  
+
+    final border=   OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2.5,
+                        style: BorderStyle.solid,                       
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                    );
+
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
           body: Center(           
             child:Column(
@@ -19,34 +28,36 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
                 ),
               
                 ),
-                TextField(
-                      style: TextStyle(
-                      color: Colors.black,
+
+                //Padding 
+                
+                Padding(
+                  padding: const EdgeInsets.all(12.10),
+                  child: TextField(
+                        style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: 'Please Enter Amount in USD',
+                        hintStyle: TextStyle(
+                          color:Colors.black,
+                      ),
+                      prefixIcon:Icon(Icons.monetization_on_outlined),
+                      prefixIconColor: Colors.black,
+                      filled: true,
+                      fillColor:Colors.white,
+                      focusedBorder: border,
+                       enabledBorder: border,                   
+                    ),     
+                   keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                    signed: true
                     ),
-                    decoration: InputDecoration(
-                      hintText: 'Please Enter Amount in USD, from zorin',
-                      hintStyle: TextStyle(
-                        color:Colors.black,
-                    ),
-                    prefixIcon:Icon(Icons.monetization_on_outlined),
-                    prefixIconColor: Colors.black,
-                    filled: true,
-                    fillColor:Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 6, 3, 103),
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                        strokeAlign: BorderSide.strokeAlignOutside,
-                              
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
-                    enabledBorder: UnderlineInputBorder()
-                    ),
-                 ), 
-                 
-                ],                
+                  
+                   ),
+                ),                    
+                                 
+              ],                
             ),  
          ),
     );
